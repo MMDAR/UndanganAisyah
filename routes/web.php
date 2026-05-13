fad17dbde79eed36c362c8521a54365c961175d6
Muhammad Amir <mmmamir420@gmail.com>
2026-05-13 21:22:28 +0700

    undangan pernikahan

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/invitation', function (Request $request) {
-    // Mengambil nama dari parameter 'to', jika kosong defaultnya 'Tamu Undangan'
-    $namaTamu = $request->query('to', 'Tamu Undangan');
+use Google\Client;
+use Google\Service\Sheets;
 
-    return view('invitation', compact('Tamu Undangan'));
-});
+Route::get('/invitation/{id}', function ($id) {
+    $client = new Client();
+    $client->setAuthConfig(json_decode(env('GOOGLE_SERVICE_ACCOUNT_JSON'), true));
+    $client->addScope(Sheets::SPREADSHEETS_READONLY);
+    
+    $service = new Sheets($client);
+    $spreadsheetId = env('GOOGLE_SHEET_ID');
+    $range = 'Sheet1!A2:B'; // Asumsi Kolom A adalah ID, Kolom B adalah Nama
 
-Route::get('/', function () {
-    return view('invitation');
-});
+    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+    $values = $response->getValues();
 
-Route::get('/invitation', function () {
-    return view('invitation');
+    $namaTamu = 'Tamu Undangan';
+    foreach ($values as $row) {
+        if ($row[0] == $id) { // Cari berdasarkan ID di kolom A
+            $namaTamu = $row[1]; // Ambil nama di kolom B
+            break;
+        }
+    }
+
+    return view('invitation', ['namaTamu' => $namaTamu]);
 });
